a8ce5c4043fe9b754518a4b9fa63bd787a36f12c
Lev Kravinsky <kravinskylev@gmail.com>
2016-03-28 18:51:45 -0400

    fix game size and edges

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,16 @@
 var ghost;
-var bg;
+var enemies;
 const color = getRandColor(5);
-var SCENE_W = window.innerWidth * 2;
-var SCENE_H = window.innerHeight * 2;
-var MARGIN = 20;
+var SCENE_W = window.innerWidth;
+var SCENE_H = window.innerHeight;
 
 function setup() {
   document.body.style.backgroundColor = "rgb(" + color.join(",") + ")";
-  createCanvas(window.innerWidth, window.innerHeight);
+  createCanvas(SCENE_W, SCENE_H);
   createGhost();
-  createAnimations();
   createEnemies();
+  createGoal();
+  createAnimations();
 }
 
 function draw() {
@@ -18,12 +18,12 @@ function draw() {
   background(color[0],color[1],color[2]);
   camera.position.x = ghost.position.x;
   camera.position.y = ghost.position.y;
-  bg.draw();
-  bg.bounce(bg, explode);
+  enemies.draw();
+  enemies.bounce(ghost, explode);
+  enemies.bounce(enemies);
   drawSprite(ghost);
-  ghost.bounce(bg);
   shadowGhost();
-  mouseIsPressed ? camera.zoom = .25 : camera.zoom = .5;
+  mouseIsPressed ? camera.zoom = .4 : camera.zoom = .75;
   checkMargins();
 }
 
@@ -35,27 +35,19 @@ function getRandColor(brightness){
   }
 
 function move() {
-  if(keyDown(RIGHT_ARROW)) {
+  if (keyDown("d")) {
     ghost.addSpeed(0.7, 0);
     ghost.changeAnimation("right");
-  }
-
-  if(keyDown(DOWN_ARROW)) {
+  } else if (keyDown("s")) {
     ghost.addSpeed(0.7, 90);
     ghost.changeAnimation("down");
-  }
-
-  if(keyDown(LEFT_ARROW)) {
+  } else if (keyDown("a")) {
     ghost.addSpeed(0.7, 180);
     ghost.changeAnimation("left");
-  }
-
-  if(keyDown(UP_ARROW)) {
+  } else if (keyDown("w")) {
     ghost.addSpeed(0.7, 270);
     ghost.changeAnimation("up");
-  }
-
-  if (!keyDown(UP_ARROW) && !keyDown(DOWN_ARROW) && !keyDown(LEFT_ARROW) && !keyDown(RIGHT_ARROW)) {
+  } else {
     ghost.changeAnimation("still");
   }
 }
@@ -64,49 +56,50 @@ function createGhost() {
   ghost = createSprite(600, 200, 50, 100);
   ghost.maxSpeed = 8;
   ghost.friction = .93;
+  ghost.mass = 50;
   ghost.setCollider("circle", 0,0, 20);
 }
 
 function createEnemies() {
-  bg = new Group();
+  enemies = new Group();
 
-  for(var i=0; i<20; i++) {
-    var enemy = createSprite(random(-width, SCENE_W+width), random(-height, SCENE_H+height));
+  for (var i = 0; i < 20; i++) {
+    var enemy = createSprite(random(0, SCENE_W), random(0, SCENE_H));
     enemy.addAnimation("normal", "/images/asterisk_stretching0001.png","/images/asterisk_stretching0008.png");
     enemy.friction = .98;
-    bg.add(enemy);
+    enemy.addAnimation("explode", "/images/asterisk_explode0001.png","/images/asterisk_explode0011.png");
+    enemies.add(enemy);
   }
 }
 
-function explode(enemy1, enemy2) {
-  setTimeout(function() {
-    enemy1.remove();
-    enemy2.remove();
-  }, 700);
-  enemy1.addAnimation("explode", "/images/asterisk_explode0001.png","/images/asterisk_explode0011.png");
-  enemy2.addAnimation("explode", "/images/asterisk_explode0001.png","/images/asterisk_explode0011.png");
+function explode(enemy1, ghost) {
+  enemy1.changeAnimation("explode")
 }
 
 function createAnimations() {
-  ghost.addAnimation("right", "/images/ghost_walk0001.png", "/images/ghost_walk0004.png")
-  ghost.addAnimation("left", "/images/ghost_walk0001left.png", "/images/ghost_walk0002left.png", "/images/ghost_walk0003left.png", "/images/ghost_walk0004left.png")
-  ghost.addAnimation("up", "/images/ghost_standing0001.png", "/images/ghost_standing0007.png")
-  ghost.addAnimation("down", "/images/ghost_standingdown0001.png", "/images/ghost_standingdown0007.png")
-  ghost.addAnimation("still", "/images/ghost_spin0001.png", "/images/ghost_spin0003.png")
+  ghost.addAnimation("right", "/images/ghost_walk0001.png", "/images/ghost_walk0004.png");
+  ghost.addAnimation("left", "/images/ghost_walk0001left.png", "/images/ghost_walk0002left.png", "/images/ghost_walk0003left.png", "/images/ghost_walk0004left.png");
+  ghost.addAnimation("up", "/images/ghost_standing0001.png", "/images/ghost_standing0007.png");
+  ghost.addAnimation("down", "/images/ghost_standingdown0001.png", "/images/ghost_standingdown0007.png");
+  ghost.addAnimation("still", "/images/ghost_spin0001.png", "/images/ghost_spin0003.png");
 }
 
 function shadowGhost() {
   noStroke();
   fill(0,0,0,20);
-  ellipse(ghost.position.x, ghost.position.y+90, 80, 30);
+  ellipse(ghost.position.x, ghost.position.y + 90, 80, 30);
 }
 
 function checkMargins() {
-  for(var i = 0; i < allSprites.length; i++) {
+  for (var i = 0; i < allSprites.length; i++) {
     var s = allSprites[i];
-    if(s.position.x < -MARGIN) s.position.x = width + MARGIN;
-    if(s.position.x > width + MARGIN) s.position.x = -MARGIN;
-    if(s.position.y < -MARGIN) s.position.y = height + MARGIN;
-    if(s.position.y > height + MARGIN) s.position.y = -MARGIN;
+    if(s.position.x < 0) s.position.x = 0;
+    if(s.position.y < 0) s.position.y = 0;
+    if(s.position.x > SCENE_W) s.position.x = SCENE_W;
+    if(s.position.y > SCENE_H) s.position.y = SCENE_H;
   }
+}
+
+function createGoal() {
+  
 }
